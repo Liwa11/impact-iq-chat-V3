0d69466c59133eb608e799410d4d0c6fcc29f1ec
Ouail <ouail.impactiq@gmail.com>
2025-04-14 23:41:55 +0200

    Feature: alle knoppen functioneel + profielpagina + chatlijst

---
+++ public/scripts/mobile.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+    const sidebar = document.querySelector('.sidebar');
+    const ins 
